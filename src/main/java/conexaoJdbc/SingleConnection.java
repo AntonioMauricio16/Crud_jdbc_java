3917f229ceb94bc224a87725112f9440782e4ae4
Antonio Mauricio <amtoniomauriciotech@gmail.com>
2023-08-06 22:53:21 -0300

    crinado projeto jdbc com java

---
+++ src/main/java/conexaoJdbc/SingleConnection.java
@@ -0,0 +1,13 @@
+package conexaoJdbc;
+
+import java.sql.Connection;
+
+public class SingleConnection {
+	
+	private static String banco = "";
+	private static String user = "";
+	private static String passwor = "";
+	private static Connection connection = null;
+	
+
+}
