f7eaf05cfd446f8276ff6ba8a2d538f2db8b6492
Antonio Mauricio <amtoniomauriciotech@gmail.com>
2023-08-06 23:20:28 -0300

    criando metodos de conecxao

--- src/main/java/conexaoJdbc/SingleConnection.java
+++ src/main/java/conexaoJdbc/SingleConnection.java
@@ -8,6 +8,29 @@ public class SingleConnection {
 	private static String user = "";
 	private static String passwor = "";
 	private static Connection connection = null;
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	
 	
 
 }
